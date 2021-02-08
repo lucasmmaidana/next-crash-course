7a6621e6bde65a3604d42dd90dc8e0700aec5cb9
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-02-07 22:10:38 -0300

    Production api route fixed

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production"
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-crash-course-qb84q0a18.vercel.app"
+  : "https://next-crash-course-cyan.vercel.app/"
